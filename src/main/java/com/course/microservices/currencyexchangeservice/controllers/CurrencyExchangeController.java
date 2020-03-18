aabbb4b099f105fe4718466a2a8542b923460e86
Demis Gomes <demismg72@gmail.com>
2020-03-18 18:57:01 -0300

    Integration with zuul

--- src/main/java/com/course/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/com/course/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -3,6 +3,7 @@ package com.course.microservices.currencyexchangeservice.controllers;
 import com.course.microservices.currencyexchangeservice.domain.ExchangeValue;
 import com.course.microservices.currencyexchangeservice.services.ExchangeValueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RestController
+@EnableDiscoveryClient
 public class CurrencyExchangeController {
     @Autowired
     private Environment environment;

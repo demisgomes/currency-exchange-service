e09c6eff2bcc302d12e7edd88cf8cc4a6815691d
Demis Gomes <demismg72@gmail.com>
2020-03-19 16:03:37 -0300

    Integration with zipkin and sleuth

--- src/main/java/com/course/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/course/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,13 +1,20 @@
 package com.course.microservices.currencyexchangeservice;
 
+import brave.sampler.Sampler;
 import com.course.microservices.currencyexchangeservice.repositories.ExchangeValueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
--- src/main/java/com/course/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/com/course/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.course.microservices.currencyexchangeservice.controllers;
 
 import com.course.microservices.currencyexchangeservice.domain.ExchangeValue;
 import com.course.microservices.currencyexchangeservice.services.ExchangeValueService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
@@ -14,6 +16,8 @@ import java.util.Objects;
 @RestController
 @EnableDiscoveryClient
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private Environment environment;
 
@@ -23,6 +27,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
         ExchangeValue exchangeValue = exchangeValueService.retrieveCurrencyFromAndTo(from, to);
+        logger.info("", exchangeValue);
         exchangeValue.setPort(
                 Integer.parseInt(
                         Objects.requireNonNull(environment.getProperty("local.server.port"))
